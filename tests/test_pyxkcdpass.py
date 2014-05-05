fd1f92cbe08769d9e8ba9540008819f3fef6f591
Henrique Pereira <ikkibr@gmail.com>
2014-05-05 09:05:12 -0300

    First commit as a package

---
+++ tests/test_pyxkcdpass.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+test_pyxkcdpass
+----------------------------------
+
+Tests for `pyxkcdpass` module.
+"""
+
+import unittest
+
+from pyxkcdpass import pyxkcdpass
+
+
+class TestPyxkcdpass(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def test_something(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
